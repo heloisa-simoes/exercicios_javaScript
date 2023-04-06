ed64157aa8ab2a4cea66f6f949b96f9b14d35686
heloisa simões <heloisasimoes9@gmail.com>
2023-04-05 23:46:24 -0300

    exercicios javascript

---
+++ exercicio 1/main.js
@@ -0,0 +1,12 @@
+let nome = 'heloisa';
+let sobreNome = 'Simões'
+let nomeEsobreNome = nome + sobreNome
+
+console.log(`É um prazer te conhecer ${nomeEsobreNome}`)
+
+//ou
+
+let perguntaNome = window.prompt("Qual é seu nome?")
+let perguntaSobrenome = window.prompt("Qual é seu sobrenome?")
+let nomeCompleto = perguntaNome + perguntaSobrenome
+console.log(nomeCompleto)
---
+++ exercicio 2/main.js
@@ -0,0 +1,12 @@
+function clicou() {
+    alert("vc clicou aqui")
+}
+
+function mover() {
+    alert("passou o mouse")
+
+}
+
+function over() {
+    alert("outro tipo de demostração")
+}
